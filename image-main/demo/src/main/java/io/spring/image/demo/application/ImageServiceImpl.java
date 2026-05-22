afe03f3bf2030e5d6b5c9b17306f07e00137181d
Andr3wsElia <andrews.elia@estudante.ifmt.edu.br>
2026-05-22 11:26:41 -0400

    22/05
    22/05/2026

--- image-main/demo/src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
+++ image-main/demo/src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
@@ -3,11 +3,13 @@ package io.spring.image.demo.application;
 
 import io.spring.image.demo.domain.entity.Image;
 import io.spring.image.demo.domain.service.ImageService;
+import io.spring.image.demo.domain.enums.ImageExtension;
 import io.spring.image.demo.infra.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,10 +21,16 @@ public class ImageServiceImpl implements ImageService {
     @Override
     @Transactional
 
-    public Image save(Image image){
+    public Image save(Image image) {
         return repository.save(image);
     }
-    public Optional<Image> getById(String id){
+
+    public Optional<Image> getById(String id) {
         return repository.findById(id);
+    }
+
+    @Override
+    public List<Image> search(ImageExtension extension, String query){
+        return repository.findByExtensionAndNameOrTagsLike(extension, query);
     }
 }
